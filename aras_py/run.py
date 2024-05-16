f23a07811985bf9181c39dd8d1b36c307e1523fb
Natanael Arndt <arndtn@gmail.com>
2024-05-16 15:32:19 +0200

    ruff format

--- aras_py/run.py
+++ aras_py/run.py
@@ -5,8 +5,9 @@ import zipfile
 import io
 from .datastructures import xml_to_list
 
+
 @click.group()
-@click.option('--base', envvar='ARAS_REST_BASE')
+@click.option("--base", envvar="ARAS_REST_BASE")
 @click.pass_context
 def cli(ctx, base):
     ctx.ensure_object(dict)
@@ -18,21 +19,21 @@ def cli(ctx, base):
 def repositories(ctx):
     """Access the ARAS interface via REST to list the available repositories."""
     with ctx.obj["client"] as client:
-        r = client.get(f'/access/repositories')
+        r = client.get(f"/access/repositories")
 
     logger.debug(f"{r.request.url}")
     print(f"{xml_to_list(r.content)}")
 
 
 @cli.command()
-@click.option('--repository', envvar='ARAS_REPO', default="warc")
-@click.option("target_path", '--path', default=".")
-@click.argument('idn')
+@click.option("--repository", envvar="ARAS_REPO", default="warc")
+@click.option("target_path", "--path", default=".")
+@click.argument("idn")
 @click.pass_context
 def get(ctx, repository, idn, target_path):
     """Access the ARAS interface via REST"""
     with ctx.obj["client"] as client:
-        r = client.get(f'/access/repositories/{repository}/artifacts/{idn}')
+        r = client.get(f"/access/repositories/{repository}/artifacts/{idn}")
         logger.debug(f"{r.headers["content-disposition"]}")
         logger.debug(f"{r.headers["content-type"]}")
         assert r.headers["content-type"] == "application/zip"
@@ -42,5 +43,5 @@ def get(ctx, repository, idn, target_path):
         z.extract(z.namelist()[0], path=target_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli(obj={})
