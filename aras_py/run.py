7f7b4257055d5375e942be8fe3106c6f703d344f
Natanael Arndt <arndtn@gmail.com>
2024-07-15 17:03:13 +0200

    Implement download of multiple files

--- aras_py/run.py
+++ aras_py/run.py
@@ -31,16 +31,23 @@ def repositories(ctx):
 @click.argument("idn")
 @click.pass_context
 def get(ctx, repository, idn, target_path):
-    """Access the ARAS interface via REST."""
-    with ctx.obj["client"] as client:
-        r = client.get(f"/access/repositories/{repository}/artifacts/{idn}")
+    """Access the ARAS interface via REST and write it to the filesystem."""
+    for name, bytes in get_bytes(ctx.obj["client"], repository, idn).items():
+        open(f"{target_path}/{idn}_{name}", mode='wb').write(bytes)
+
+
+def get_bytes(client, repository, idn):
+    """Access the ARAS interface via REST.
+    Get the WARC files of an IDN as bytes."""
+    with client as connection:
+        r = connection.get(f"/access/repositories/{repository}/artifacts/{idn}")
         logger.debug(f"{r.headers["content-disposition"]}")
         logger.debug(f"{r.headers["content-type"]}")
         assert r.headers["content-type"] == "application/zip"
         z = zipfile.ZipFile(io.BytesIO(r.content))
         logger.debug(f"{z.namelist()}")
-        assert len(z.namelist()) == 1
-        z.extract(z.namelist()[0], path=target_path)
+        logger.debug(f"{z.infolist()}")
+        return {member.filename: z.read(member) for member in z.infolist()}
 
 
 if __name__ == "__main__":
