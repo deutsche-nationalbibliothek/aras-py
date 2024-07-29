3060c0f8b43d719a824fb4380f869dcdf8f356d8
Natanael Arndt <arndtn@gmail.com>
2024-07-29 15:03:35 +0200

    Add test for get_stream method

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,24 +1,64 @@
 from loguru import logger
-from httpx import Client
-from pathlib import Path
+from httpx import Response
+from textwrap import dedent
+import pytest
 
 from aras_py.run import get_stream
 
+test_base_url = "http://dnb-test-aras/"
 
-def test_aras_get_stream(tmp_path):
+@pytest.mark.respx(base_url=test_base_url)
+def test_aras_get_stream(respx_mock):
 
-    client = Client(base_url="http://localhost:8080/")
+    example_content_0 = "One example WARC content"
+    example_content_1 = "Another example WARC content"
 
-    repository = "example"
-    idn = "123"
+    repository = "example_warc"
+    idn = "1234567890"
 
-    target_path = tmp_path
-    target_path = Path("/tmp/aras")
-    #/access/repositories/example/artifacts/123
+    mets_file = """
+    <file ID="{id}" MIMETYPE="{mime_type}" CREATED="2018-10-01T07:15:08" SIZE="{size}">
+        <FLocat LOCTYPE="URL" xlink:href="{href}"/>
+    </file>
+    """
+    # id = 0
+    # mime_type = "application/gzip"
+    # size = 180641790
+    # href = "DNB-oia-02879-20180502192019-66e7eed6-3d4e-e811-a3f5-d4ae528b7600-00000-DWA-DA2.warc.gz"
 
+    mock_files = [
+        mets_file.format(id=0, mime_type="text/plain", size=len(example_content_0.encode("utf8")), href="example_0.txt"),
+        mets_file.format(id=1, mime_type="text/plain", size=len(example_content_1.encode("utf8")), href="example_1.txt")
+    ]
 
-    for name, bytes_io in get_stream(client, repository, idn):
-        with open(f"{target_path}/{idn}_{name}", mode='wb') as target:
-            target.write(bytes_io.read())
+    #/access/repositories/example_warc/artifacts/1234567890/objects
+    respx_mock.get(f"/access/repositories/{repository}/artifacts/{idn}/objects").mock(return_value=Response(200, text=dedent(f"""
+        <?xml version='1.0' encoding='UTF-8'?>
+        <mets xmlns="http://www.loc.gov/METS/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/mets.xsd">
+            <metsHdr CREATEDATE="2024-07-29T12:32:44" RECORDSTATUS="draft">
+                <agent ROLE="CREATOR" TYPE="ORGANIZATION">
+                    <name>Deutsche Nationalbibliothek</name>
+                </agent>
+            </metsHdr>
+            <fileSec>
+                <fileGrp>
+                    {"".join({file for file in mock_files})}
+                </fileGrp>
+            </fileSec>
+            <structMap>
+                <div/>
+            </structMap>
+        </mets>
+        """).strip()))
 
-    assert len(list(tmp_path.iterdir())) == 2
+    respx_mock.get(f"/access/repositories/{repository}/artifacts/{idn}/objects/0").mock(return_value=Response(200, text=example_content_0))
+    respx_mock.get(f"/access/repositories/{repository}/artifacts/{idn}/objects/1").mock(return_value=Response(200, text=example_content_1))
+
+    stream_iter = get_stream(test_base_url, repository, idn)
+
+    name, stream, options = next(stream_iter)
+    with stream() as bytes_io:
+        bytes_io.read() == example_content_0.encode("utf-8")
+    name, stream, options = next(stream_iter)
+    with stream() as bytes_io:
+        bytes_io.read() == example_content_1.encode("utf-8")
